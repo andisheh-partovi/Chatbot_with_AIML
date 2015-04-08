46bed9288159d03c336d79ced0f70bd041940d9f
andisheh <andisheh.dysh.partovi@gmail.com>
2015-04-08 10:44:44 +1000

    updated aiml and python script

--- pyaiml3-master/pyaiml3-master/ass2.py
+++ pyaiml3-master/pyaiml3-master/ass2.py
@@ -4,6 +4,11 @@ sys.path.append('./aiml')
 import aiml
 k = aiml.Kernel()
 k.learn('consultant.aiml')
+
+# set a constant
+k.setBotPredicate("name", "Mr.White")
+k.setBotPredicate("city", "Melbourne")
+
 while True: 
-    response = k.respond(input("consultant> "))
+    response = k.respond(input("User> "))
     print(response)
