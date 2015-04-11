312b50d4848a3e6a5e68df93ef2d25c47b77f71f
andisheh <andisheh.dysh.partovi@gmail.com>
2015-04-11 10:36:04 +1000

    added more energy stuff to sheldon

--- pyaiml3-master/pyaiml3-master/assigment2.py
+++ pyaiml3-master/pyaiml3-master/assigment2.py
@@ -3,7 +3,7 @@ sys.path.append('./aiml')
 
 import aiml
 k = aiml.Kernel()
-k.learn('sheldon_scripted.aimlhow are you')
+k.learn('sheldon.aiml')
 
 # set a constant
 k.setBotPredicate("name", "Sheldon Cooper")
