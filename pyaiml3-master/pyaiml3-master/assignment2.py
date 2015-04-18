c412ebadb4eb3259d52469074981aa3e020f272f
andisheh <andisheh.dysh.partovi@gmail.com>
2015-04-18 21:58:26 +1000

    Modified the added dialogue samples

---
+++ pyaiml3-master/pyaiml3-master/assignment2.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('./aiml')
+
+import aiml
+k = aiml.Kernel()
+k.learn('sheldon.aiml')
+
+# set a constant
+k.setBotPredicate("name", "Sheldon Cooper")
+k.setBotPredicate("city", "Melbourne")
+k.setBotPredicate("houseHoldMemberNumbers", "2")
+k.setBotPredicate("houseHoldMember", "Leonard")
+k.setBotPredicate("age", "30")
+k.setBotPredicate("friendAge", "32")
+k.setBotPredicate("job", "student")
+k.setBotPredicate("weekdaysHours", "14")
+k.setBotPredicate("weekendsHours", "24")
+
+while True: 
+    response = k.respond(input("User> "))
+    print(response)
