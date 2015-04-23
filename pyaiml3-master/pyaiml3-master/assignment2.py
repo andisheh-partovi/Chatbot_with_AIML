79c8d9c6e70689de1ef0e21fdfb6e0a2b43aff1c
andisheh <andisheh.dysh.partovi@gmail.com>
2015-04-23 13:10:00 +1000

    added stuff

--- pyaiml3-master/pyaiml3-master/assignment2.py
+++ pyaiml3-master/pyaiml3-master/assignment2.py
@@ -15,9 +15,23 @@ k.setBotPredicate("friendAge", "32")
 k.setBotPredicate("job", "student")
 k.setBotPredicate("weekdaysHours", "14")
 k.setBotPredicate("weekendsHours", "24")
-k.setBotPredicate("summerBill", "$300")
-k.setBotPredicate("winterBill", "$200")
+k.setBotPredicate("summerBill", "$30")
+k.setBotPredicate("winterBill", "$20")
 
-while True: 
-    response = k.respond(input("User> "))
-    print(response)
+
+# open the log file
+logfile = open("log.txt", 'a')
+
+#Enter the main input/output loop
+print("type exit to quit")
+user = input("consultant> ")
+while(user != "exit" ):
+	logfile.write("U: "+str(user)+"\r")
+	agent = k.respond(user)
+	print(agent)
+	user = input("consultant> ")
+	logfile.write("B: "+str(agent)+"\r")
+	logfile.flush()
+	
+logfile.write("------------------------"+"\r\n")
+logfile.close()
